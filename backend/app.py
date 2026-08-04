621eb5c7595a2157bed7274c437dc1abfed4b2d6
samidha <samidhawani7@gmail.com>
2026-08-04 18:26:13 +0530

    Update home route response message

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "Tic Tac Toe Backend Running from ec2 instance by myself"
+    return "Tic Tac Toe Backend Running"
 
 
 @app.route("/move", methods=["POST"])
