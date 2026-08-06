2d39961a051b9e8933b0e9fd3d4a13f959e03284
samidha <samidhawani7@gmail.com>
2026-08-06 16:45:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "Tic Tac Toe Backend Running from ec2 instnace by samidha nitin wani"
+    return "Tic Tac Toe Backend Running on EC2"
 
 
 @app.route("/move", methods=["POST"])
