272f8872f3a8db9fa964aab9e8151c7a2816d14f
samidha <samidhawani7@gmail.com>
2026-08-06 13:19:22 +0530

    Update home route message to include EC2 instance infro

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "Tic Tac Toe Backend Running"
+    return "Tic Tac Toe Backend Running from ec2 instnace by samidha wani"
 
 
 @app.route("/move", methods=["POST"])
